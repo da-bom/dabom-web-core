5371008fd1242a0da86aec7423ce2cd5bd224775
tray0244 <tray024400@gmail.com>
2026-02-18 17:19:01 +0900

    DABOM-190 feat: 아이콘 추가

---
+++ packages/shared/src/assets/icons/Stop.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgStop = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={12} height={12} fill="none" {...props}><path fill="#565656" d="M6 0C2.688 0 0 2.688 0 6s2.688 6 6 6 6-2.688 6-6-2.688-6-6-6M1.2 6c0-2.652 2.148-4.8 4.8-4.8 1.11 0 2.13.378 2.94 1.014L2.214 8.94A4.74 4.74 0 0 1 1.2 6M6 10.8c-1.11 0-2.13-.378-2.94-1.014L9.786 3.06A4.74 4.74 0 0 1 10.8 6c0 2.652-2.148 4.8-4.8 4.8" /></svg>;
+export default SvgStop;
