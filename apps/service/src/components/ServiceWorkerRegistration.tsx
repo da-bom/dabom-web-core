7778e715c4de10c05fe2b95ec0b1a674b19bdf2e
swthewhite <swthewhite@gmail.com>
2026-03-04 14:29:58 +0900

    DABOM-357 fix: .catch() 에러 핸들링 추가

--- apps/service/src/components/ServiceWorkerRegistration.tsx
+++ apps/service/src/components/ServiceWorkerRegistration.tsx
@@ -5,7 +5,9 @@ import { useEffect } from 'react';
 export default function ServiceWorkerRegistration() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js');
+      navigator.serviceWorker.register('/sw.js').catch((error) => {
+        console.error('Service Worker registration failed: ', error);
+      });
     }
   }, []);
 
