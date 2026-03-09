0881fcd8c298ab0efd3776aab6615b2124b2c970
tray0244 <tray024400@gmail.com>
2026-03-09 11:17:56 +0900

    DABOM-388 feat: not found, expire 화면 추가

---
+++ apps/service/src/app/not-found.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Button, DaboIcon } from '@shared';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  const handleGoHome = () => {
+    router.push('/home');
+  };
+
+  return (
+    <main className="flex h-dvh w-full flex-col items-center justify-center px-7.75">
+      <div className="flex flex-col items-center">
+        <DaboIcon type="hurt" />
+
+        <p className="text-body2-d mt-6 text-center">앗, 접근할 수 없는 페이지예요.</p>
+
+        <div className="mt-29.5">
+          <Button type="button" size="lg" color="dark" onClick={handleGoHome}>
+            홈으로 돌아가기
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+}
