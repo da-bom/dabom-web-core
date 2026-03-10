148769c1096bb87e1e2fca67cbf94c1092a7641e
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-03-11 02:11:49 +0900

    DABOM-334 feat: formatRewardProduct 모든 key가 고유하도록 수정

--- apps/admin/src/utils/formatRewardProduct.tsx
+++ apps/admin/src/utils/formatRewardProduct.tsx
@@ -9,14 +9,16 @@ export const formatRewardProduct = ({ product }: { product: readonly Product[] }
   return product.map((p) => ({
     id: p.id,
     cells: [
-      <span key={p.id}>{p.id}</span>,
-      <span key={p.id}>{p.type === 'DATA' ? '데이터' : '기프티콘'}</span>,
-      <div key={p.id}>{p.type === 'DATA' ? '-' : <ImageIcon />}</div>,
-      <span key={p.id}>{p.productName}</span>,
-      <span key={p.id}>{p.price.toLocaleString()}</span>,
-      <Link key={p.id} className="flex justify-center" href={`/reward/products/${p.id}`}>
+      <span key={`${p.id}-id`}>{p.id}</span>,
+      <span key={`${p.id}-type`}>{p.type === 'DATA' ? '데이터' : '기프티콘'}</span>,
+      <div key={`${p.id}-icon`} className="flex justify-center">
+        {p.type === 'DATA' ? '-' : <ImageIcon />}
+      </div>,
+      <span key={`${p.id}-name`}>{p.productName}</span>,
+      <span key={`${p.id}-price`}>{p.price.toLocaleString()}</span>,
+      <Link key={`${p.id}-link`} className="flex justify-center" href={`/reward/products/${p.id}`}>
         <Button size="sm" color="light">
-          수정
+          <span>수정</span>
         </Button>
       </Link>,
     ],
