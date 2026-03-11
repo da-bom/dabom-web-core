0a53718390a4bfe407520089ba19fe2fd97eb9a2
tray0244 <tray024400@gmail.com>
2026-03-11 18:44:25 +0900

    DABOM-328 refactor: 텍스트 수정

--- apps/service/src/app/(afterLogin)/recap/RecapStep2Time.tsx
+++ apps/service/src/app/(afterLogin)/recap/RecapStep2Time.tsx
@@ -15,15 +15,14 @@ export function RecapStep2Time({ startHour, endHour }: RecapStep2TimeProps) {
   const isMorning = startHour >= 6 && startHour < 18;
 
   return (
-    <div className="relative flex flex-1 flex-col overflow-hidden transition-colors duration-500">
+    <div className="relative flex flex-1 flex-col">
       <div className="mt-[29px] pl-[27px]">{isMorning ? <DaboSun /> : <DaboMoon />}</div>
 
       <div className="mt-[114px] pl-[39px]">
         <h1
-          className={cn(
-            'text-h1-m w-fit break-keep whitespace-pre-line',
-            isMorning ? 'text-brand-black' : 'text-brand-white',
-          )}
+          className={`text-h1-m w-fit leading-[29px] break-keep whitespace-pre-line ${
+            isMorning ? 'text-brand-black' : 'text-brand-white'
+          }`}
         >
           {startHour}~{endHour}
           {isMorning ? RECAP_UI_TEXT.STEP2_MORNING_SUFFIX : RECAP_UI_TEXT.STEP2_NIGHT_SUFFIX}
