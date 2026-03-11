8dee52d0fd2fb8467f4d9759786f2d56f17f2994
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-03-11 14:19:04 +0900

    DABOM-334 feat: 인터셉트 구조 수정

--- apps/admin/src/app/(afterLogin)/reward/products/create/@modal/(..)[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import RewardCreateDrawer from 'src/components/reward/RewardCreateDrawer';
-
-const Page = () => {
-  return <RewardCreateDrawer />;
-};
-
-export default Page;
--- apps/admin/src/app/(afterLogin)/reward/products/page.tsx
+++ apps/admin/src/app/(afterLogin)/reward/products/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { Button, Table } from '@shared';
 
 import { REWARD_PRODUCT } from 'src/data/reward';
@@ -6,9 +8,11 @@ import { formatRewardProduct } from 'src/utils/formatRewardProduct';
 const RewardProductPage = () => {
   return (
     <div className="flex flex-col gap-4">
-      <Button size="lg" color="light">
-        보상 추가하기
-      </Button>
+      <Link href="/reward/products/create">
+        <Button size="lg" color="light">
+          보상 추가하기
+        </Button>
+      </Link>
       <Table
         className="rounded-md"
         headers={['ID', '유형', '썸네일', '상품', '단가', '관리']}
