03a0f7ebb8ebcbdb8c25a491b8613c253228fa40
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-03-15 01:15:37 +0900

    DABOM-393 feat: dev tool 환경변수 추가

--- apps/service/src/app/providers.tsx
+++ apps/service/src/app/providers.tsx
@@ -11,11 +11,12 @@ export function Providers({
   children: Readonly<React.ReactNode>;
 }>) {
   const [queryClient] = useState(() => new QueryClient());
+  const showDevtools = process.env.NEXT_PUBLIC_SHOW_QUERY_DEVTOOLS === 'true';
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-left" />
+      {showDevtools && <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-left" />}
     </QueryClientProvider>
   );
 }
