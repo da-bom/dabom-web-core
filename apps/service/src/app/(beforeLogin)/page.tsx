e43313dd972afffcf47817a3d3f38a9429105884
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-03-20 09:14:43 +0900

    DABOM-516 feat: 랜딩 애니메이션 추가

--- apps/service/src/app/(beforeLogin)/page.tsx
+++ apps/service/src/app/(beforeLogin)/page.tsx
@@ -16,7 +16,7 @@ export default function SignupPage() {
     <div className="flex min-h-screen flex-col items-center">
       <main className="mt-65 flex flex-col items-center">
         <div className="flex flex-col items-center gap-7">
-          <div className="flex items-center justify-center">
+          <div className="flex animate-bounce items-center justify-center">
             <DaboIcon type="default" sx={{ width: '130px', height: '130px' }} />
           </div>
 
